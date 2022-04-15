264714d77baa9ef9c09ff3c3629cf095dd75acd9
Asim-Coding <muhammadasim19955@gmail.com>
2022-04-15 11:06:31 +0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  data="x";
+  updateDataEvent(item:String){
+    console.warn(item)
+    this.data=item;
+  }
 }
